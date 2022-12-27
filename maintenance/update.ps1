256bbcd55ca924ef42226cc696ce5ce7ea923260
Michal Žamboch <zamboch.michal@seznam.cz>
2022-12-27 09:35:08 +0100

    win update

---
+++ maintenance/update.ps1
@@ -0,0 +1,11 @@
+# Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
+
+if (-Not (Get-Module -ListAvailable -Name "PSWindowsUpdate")) {
+    Write-Host "Installing PowerShell Windows Update"
+    Install-Module PSWindowsUpdate
+    Add-WUServiceManager -MicrosoftUpdate
+}
+
+Write-Host ""
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+Write-Host "Windows Update is done."
