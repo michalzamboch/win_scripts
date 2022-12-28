72c554f01784a324d9d2f48f96bd4057362efb46
Michal Žamboch <zamboch.michal@seznam.cz>
2022-12-28 17:08:06 +0100

    path vcpkg

--- reinstall/windows-install.py
+++ reinstall/windows-install.py
@@ -149,6 +149,7 @@ ls = [
     "git clone https://github.com/Microsoft/vcpkg.git C:\\dev\\vcpkg",
     "C:\\dev\\vcpkg\\bootstrap-vcpkg.bat -disableMetrics",
     "C:\\dev\\vcpkg\\vcpkg integrate install",
+    "$env:Path += \";C:\\dev\\vcpkg\""
 ]
 vcpkg = SimpleSerializer(ls, "vcpkg")
 
