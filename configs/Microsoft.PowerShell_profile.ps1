2d23f39587773f83e2ad7912e48646087ae28664
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-05 13:12:25 +0200

    powershell profile

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,25 @@
 
-# Customized terminal
-Invoke-Expression (&starship init powershell)
-Import-Module Terminal-Icons
+# Starship 
+if (Get-Command starship) {
+  Invoke-Expression (&starship init powershell)
+}
+
+# Import-Module Terminal-Icons
 
 # Custom aliases
-function sup {scoop update --all; scoop cleanup --cache --all}
-
-Set-Alias sup-dude sup
 Set-Alias -Name ll -Value Get-ChildItem
-Set-Alias -Name cat -Value Get-Content 
+Set-Alias -Name empty-trash -Value Clear-RecycleBin
+Set-Alias -Name cat -Value Get-Content
+
+# Chocolatey
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Scoop completion
+if (Get-Command scoop) {
+  function sup {scoop update --all; scoop cleanup --cache --all}
+  Set-Alias sup-dude sup
+  Import-Module scoop-completion
+}
