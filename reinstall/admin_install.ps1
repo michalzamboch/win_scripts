6512ac9e7885aed078619972f7578a684f85838c
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-06 12:38:20 +0200

    User and admin installs

---
+++ reinstall/admin_install.ps1
@@ -0,0 +1,4 @@
+..\maintenance\update.ps1
+..\modules\install_choco.ps1
+..\modules\install_choco_programs.ps1
+..\modules\install_wsl.ps1
---
+++ reinstall/full_install.ps1
@@ -0,0 +1,4 @@
+
+.\user_install.ps1
+
+sudo .\admin_insatll.ps1
