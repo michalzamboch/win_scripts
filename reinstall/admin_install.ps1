a374a200b2b4c85919ca44cb4ff50f2168840e72
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-09 10:52:45 +0200

    Moved admin scripts.

--- reinstall/admin_install.ps1
+++ reinstall/admin_install.ps1
@@ -1,4 +1,6 @@
 ..\maintenance\update.ps1
+..\modules\install_winget_programs.ps1
 ..\modules\install_choco.ps1
 ..\modules\install_choco_programs.ps1
 ..\modules\install_wsl.ps1
+..\maintenance\remove_bloatware.ps1
--- reinstall/user_install.ps1
+++ reinstall/user_install.ps1
@@ -14,11 +14,8 @@ function write_log ([string] $message) {
     Write-host $([datetime]::Now) $message -ForegroundColor Red
 }
 
-..\modules\install_winget_programs.ps1
 ..\modules\install_scoop.ps1
 ..\modules\install_scoop_programs.ps1
-..\maintenance\remove_bloatware.ps1
 ..\modules\custom_software.ps1
 ..\modules\set_profile.ps1
-..\maintenance\common_setup.ps1
 
