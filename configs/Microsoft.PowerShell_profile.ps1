055fc2b6ee1f192dea25027e088ef0f11299851f
Michal Žamboch <zamboch.michal@seznam.cz>
2023-07-09 10:21:50 +0200

    Modified powershell profile.

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -4,12 +4,25 @@ if (Get-Command starship) {
   Invoke-Expression (&starship init powershell)
 }
 
-# Import-Module Terminal-Icons
+Import-Module Terminal-Icons
 
 # Custom aliases
 Set-Alias -Name ll -Value Get-ChildItem
 Set-Alias -Name empty-trash -Value Clear-RecycleBin
 Set-Alias -Name cat -Value Get-Content
+
+# Find programming folder
+function cdp {
+  if (Test-Path "~\programming") {
+    cd ~\programming
+    return
+  }
+  if (Test-Path "D:\programming") {
+    cd D:\programming  
+    return
+  }
+  Write-Host "Could not find programming folder." -ForegroundColor Cyan
+}
 
 # Chocolatey
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -21,5 +34,6 @@ if (Test-Path($ChocolateyProfile)) {
 if (Get-Command scoop) {
   function sup {scoop update --all; scoop cleanup --cache --all}
   Set-Alias sup-dude sup
+  
   Import-Module scoop-completion
 }
