ab52136171c503fe6c4185e38d15e2f87ccb0900
Michal Žamboch <zamboch.michal@seznam.cz>
2023-09-16 17:03:19 +0200

    python watchdog

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -38,3 +38,36 @@ if (Get-Command scoop) {
   
   Import-Module scoop-completion
 }
+
+# Watch Dog
+$watchdog_script=
+@"
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+def watch(path):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stop")
+    finally:
+        observer.stop()
+        observer.join()
+
+
+watch(sys.argv[1] if len(sys.argv) > 1 else '.')
+"@
+
+function watchdog { python3 -c $watchdog_script }
